390df302713e0fa048b6a029b1a2a2e6d69f0f71
delchiaro-bh <Riccardo.DelChiaro@bakerhughes.com>
2023-03-09 11:08:30 +0100

    refact: improved CocoDataFrame test using AAA pattern

---
+++ tests/unit/test_cocohelper/test_cocodataframe.py
@@ -0,0 +1,34 @@
+from cocohelper import COCOHelper
+import pytest
+from cocohelper.dataframe import COCODataFrame
+
+
+
+
+class TestCocoDataframe:
+
+    @pytest.fixture
+    def df_imgs(self) -> COCODataFrame:
+        # Arrange
+        ch = COCOHelper.load_json('tests/data/coco_dataset/annotations/coco.json')
+        yield ch.imgs
+
+    def test_to_dict_not_having_index_column(self, df_imgs):
+        # Act:
+        imgs_dict = df_imgs.to_dict()
+
+        # Assert:
+        for column in df_imgs.columns:
+            assert column in imgs_dict.keys()
+        assert 'image_id' not in imgs_dict.keys()
+
+
+
+    def test_to_dict_include_index_column(self, df_imgs):
+        # Act:
+        imgs_dict = df_imgs.to_dict(include_index=True)
+
+        # Assert
+        for column in df_imgs.columns:
+            assert column in imgs_dict.keys()
+        assert 'image_id' in imgs_dict.keys()
